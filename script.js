a27cc4979628679d8c43e37916112419a076ad95
qwe <93801635+nkbih@users.noreply.github.com>
2023-05-03 18:31:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
 
     const style = document.createElement('style');
     style.innerHTML = `
-
         /* Set the flex-basis for the left panel */
         #__next > div.overflow-hidden.w-full.h-full.relative.flex > div.dark.hidden.bg-gray-900.md\\:flex.md\\:w-\\[260px\\].md\\:flex-col {
             flex-basis: 350px !important;
@@ -25,7 +24,14 @@
 
         .group .text-base {
             padding-left: 100px !important;
-            padding-right: 400px !important;
+            padding-right: 100px !important;
+            white-space: pre-wrap !important;
+            word-break: break-all !important;
+        }
+
+        /* Set fixed width for the chat container */
+        #__next > div.overflow-hidden.w-full.h-full.relative.flex > div.flex.h-full.max-w-full.flex-1.flex-col > main > div.flex-1.overflow-hidden > div > div > div {
+            width: calc(100% - 300px) !important; /* Adjust the value '100px' according to your desired width */
         }
     `;
 
