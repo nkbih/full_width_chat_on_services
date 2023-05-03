1395839d98e6354467cd0837c2330ebc29c09fc2
qwe <93801635+nkbih@users.noreply.github.com>
2023-05-03 18:16:30 +0400

    Update script.js
    воо, разобрался

--- script.js
+++ script.js
@@ -20,12 +20,12 @@
         }
 
         .text-base {
-            max-width: 80% !important;
+            max-width: 100% !important;
         }
 
         .group .text-base {
             padding-left: 100px !important;
-            padding-right: 100px !important;
+            padding-right: 400px !important;
         }
     `;
 
